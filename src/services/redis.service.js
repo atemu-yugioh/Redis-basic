c03a0df9455fb53f5814d68562f335ffcb4b1aeb
Nguyen_Huu_Thieng <16521532@gm.uit.edu.vn>
2023-08-01 15:27:13 +0700

    Setup Redis Service

---
+++ src/services/redis.service.js
@@ -0,0 +1,36 @@
+const { redisClient } = require('../dbs/init.redis.new')
+
+class RedisService {
+  static setKey = async ({ key, value }) => {
+    return await redisClient.set(key, value)
+  }
+
+  static getKey = async (key) => {
+    return await redisClient.get(key)
+  }
+
+  static incr = async ({ key }) => {
+    return await redisClient.incr(key)
+  }
+
+  static incrBy = async ({ key, increment }) => {
+    return await redisClient.incrBy(key, increment)
+  }
+
+  static expire = async ({ key, time }) => {
+    return await redisClient.expire(key, time)
+  }
+
+  static setNx = async ({ key, value }) => {
+    return await redisClient.setNX(key, value)
+  }
+
+  static setExNx = async ({ key, value, time }) => {
+    return await redisClient.set(key, value, {
+      EX: time,
+      NX: true
+    })
+  }
+}
+
+module.exports = RedisService
